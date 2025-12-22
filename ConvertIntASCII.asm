736865ea5a19470ad10b012fe7d8e07f222b6474
AnisDjerrab <anisdjerrab@gmail.com>
2025-12-22 16:12:37 +0000

    Convertion ASCII => Integer

--- ConvertIntASCII.asm
+++ ConvertIntASCII.asm
@@ -1,8 +1,11 @@
 section .data
     Numbers dq 8956, -99985, 665, 5112, 0
     len db 5
+    AsciiNumbers db '+1321', 0, '-744', 0, '+2633', 0, '+744', 0, '-877', 0
+    
 section .bss
     numbersResultsBuffer resb 100
+    AsciiNumbersResultsList resq 5 
 section .text
     default rel
 
@@ -11,6 +14,7 @@ _start:
     ; first part : convert an INT in ASCII
     ; first, divide the number
     mov cl, byte [len]
+    dec cl
     ; r8 will contain the index in the Result Buffer
     mov r8, 0
     ; r9 will contain the index in the Numbers list
@@ -63,3 +67,54 @@ WriteInAsciiLoop:
     dec rcx
     jmp BigLoop
 Continue:
+    ; now, convert a bunch of ASCII numbers into Integers
+    mov cl, Byte [len]
+    ; r8 will contain the index in 'AsciiNumbers'
+    mov r8, 0
+    ; r9 will contain the number we're in 
+    mov r9, 0
+    ; r10b will contain the sign of the number
+    mov r10, 0
+ConvertionLoop:
+    mov r10b, byte [AsciiNumbers + r8]
+    inc r8
+    xor rax, rax
+    mov rbx, 1
+    xor r11, r11
+    mov r12, rcx
+    xor rcx, rcx
+PushLoop:
+    mov al, byte [AsciiNumbers + r8]
+    inc r8
+    cmp al, 0
+    je PopLoop
+    push rax
+    inc rcx
+    jmp PushLoop
+PopLoop:
+    pop rax
+    sub rax, '0'
+    mul rbx
+    add r11, rax
+    mov rax, 10
+    mul rbx
+    mov rbx, rax
+    loop PopLoop
+    ; now, inverse the numbre if negative
+    cmp r10b, '-'
+    je Negative
+    jmp End
+Negative:
+    neg r11
+    jmp End 
+End:
+    ; now, write the number
+    mov qWord [AsciiNumbersResultsList + r9*8], r11
+    inc r9
+    ; restore rcx
+    mov rcx, r12
+    loop ConvertionLoop
+    ; finally, proprely exit the program
+    mov rax, 60
+    xor rdi, rdi
+    syscall
